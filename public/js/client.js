ffcdecf3db2b0d24952e6ad0199443e5962b2cb4
李凡 <lf2941270@163.com>
2015-02-04 22:31:30 +0800

    signup and signin view

--- public/js/client.js
+++ public/js/client.js
@@ -43,8 +43,7 @@ $.fn.ableBtn = function(){
 $.fn.isDisabled = function(){
 	return this.hasClass("disable");
 }
-function login(redirectUrl){
-	$("#loginModal").modal({});
+function signIn(redirectUrl){
 	$(".form-signin").submit(function(){
 		var subBtn = $(this).find("button[type=submit]");
 		if(subBtn.isDisabled()){
@@ -56,32 +55,62 @@ function login(redirectUrl){
 		}
 		$(this).clearFormError();
 		subBtn.disableBtn().text("登录中...");
-		subBtn.ableBtn().text("登录");
+		AV.User.logIn($(this).find('[name=username]').val(), $(this).find('[name=password]').val(), {
+			success: function(user) {
+				// Do stuff after successful login.
+				alert('登录成功')
+			},
+			error: function(user, error) {
+				// The login failed. Check error to see why.
+				subBtn.ableBtn().text("登录");
+			}
+		});
 
 		return false;
 	});
 }
 function signUp(){
-	var user = new AV.User();
-	user.set("username", $('[name=username]').val());
-	user.set("password", $('[name=password]').val());
-	user.set("email", $('[name=email]').val());
+
+	$(".form-signup").submit(function(){
+		var subBtn = $(this).find("button[type=submit]");
+		if(subBtn.isDisabled()){
+			return false;
+		}
+		$(this).checkForm();
+		if(!this.valid){
+			return false;
+		}
+		$(this).clearFormError();
+		subBtn.disableBtn().text("注册中...");
+		var user = new AV.User();
+		user.set("username", $(this).find('[name=username]').val());
+		user.set("password", $(this).find('[name=password]').val());
+//		user.set("email", $('[name=email]').val());
 
 // other fields can be set just like with AV.Object
 //	user.set("phone", "415-392-0202");
 
-	user.signUp(null, {
-		success: function(user) {
-			// Hooray! Let them use the app now.
-		},
-		error: function(user, error) {
-			// Show the error message somewhere and let the user try again.
-			alert("Error: " + error.code + " " + error.message);
-		}
+		user.signUp(null, {
+			success: function(user) {
+				// Hooray! Let them use the app now.
+				alert('注册成功')
+			},
+			error: function(user, error) {
+				// Show the error message somewhere and let the user try again.
+				alert("Error: " + error.code + " " + error.message);
+				subBtn.ableBtn().text("立即注册");
+
+			}
+		});
+
+		return false;
 	});
+
 }
 $(function(){
 	$('#signin').click(function(){
-		login();
+		$("#loginModal").modal({});
 	});
+	signUp();
+	signIn();
 })
